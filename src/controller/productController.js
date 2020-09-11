80b967e2a5e081906d0c36695c8d6952272819a3
Wahyu Ardiansyah <wahyuardi027@gmail.com>
2020-09-11 14:15:05 +0700

    CRUD product

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -5,9 +5,8 @@ module.exports = {
     getProduct: async (req, res) => {
         try {
             const { rows } = await query(
-                `SELECT * FROM items`
+                `SELECT * FROM items ORDER BY name ASC`
             )
-            console.log(rows);
             res.send(rows);
         } catch (error) {
             console.log(error);
@@ -16,7 +15,6 @@ module.exports = {
 
     addProduct: async (req, res) => {
         const { name, qyt, price_buy, price_sale, unit_id } = req.body;
-
         try {
             const { rows } = await query(
                 `INSERT INTO items (name, qyt, price_buy, price_sale, unit_id)
@@ -28,10 +26,62 @@ module.exports = {
             const dbResponse = rows[0];
 
             successMessage.data = dbResponse;
-            successMessage.message = 'successfully created product';
-            return res.status(status.created).send(successMessage);
+            successMessage.message = 'Berhasil menambahkan produk';
+            res.status(status.created).send(successMessage);
+        } catch (error) {
+            errorMessage.message = 'Gagal menambahkan produk';
+            errorMessage.error = error;
+            res.status(status.error).send(errorMessage);
+        }
+    },
+
+    updateProduct: async (req, res) => {
+        const id = req.params.id
+        const { name, qyt, price_buy, price_sale, unit_id } = req.body;
+
+        try {
+            const update = await query(
+                `UPDATE items SET (name, qyt, price_buy, price_sale, unit_id) = 
+                ($1, $2, $3, $4, $5) WHERE id = $6 returning *`,
+                [name, qyt, price_buy, price_sale, unit_id, id]
+            );
+            successMessage.message = 'Berhasil memperbarui produk';
+            res.status(status.success).send(successMessage);
+        } catch (error) {
+            errorMessage.message = 'Gagal memperbarui produk';
+            errorMessage.error = error;
+            res.status(status.error).send(errorMessage);
+        }
+    },
+
+    searchProduct: async (req, res) => {
+        const name = req.query.name;
+
+        try {
+            const { rows } = await query(
+                `SELECT * FROM items WHERE name = $1`,
+                [name]
+            );
+            res.send(rows);
         } catch (error) {
             console.log(error);
+        }
+    },
+
+    deleteProduct: async (req, res) => {
+        const id = req.params.id;
+
+        try {
+            const del = await query(
+                `DELETE FROM items WHERE id=$1 returning *`,
+                [id]
+            );
+            successMessage.message = 'Berhasil menghapus produk';
+            res.status(status.success).send(successMessage);
+        } catch (error) {
+            errorMessage.message = 'Gagal menghapus produk';
+            errorMessage.error = error;
+            res.status(status.error).send(errorMessage);
         }
     }
 }
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getProduct, addProduct } = require('../controller/productController');
+const { getProduct, addProduct, updateProduct, searchProduct, deleteProduct } = require('../controller/productController');
 
 router.get('/product', getProduct);
+router.post('/product', addProduct);
+router.put('/product/:id', updateProduct);
+router.get('/product/search', searchProduct);
+router.delete('/product/delete/:id', deleteProduct);
 
 module.exports = router;
